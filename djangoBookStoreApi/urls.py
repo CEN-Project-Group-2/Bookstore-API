6bb32c4b393da11c64b9c16daa9c6acc71f9586e
Tajae1532 <andersontajae7@gmail.com>
2023-07-18 17:55:31 -0700

    Get and Put Method

--- djangoBookStoreApi/urls.py
+++ djangoBookStoreApi/urls.py
@@ -1,17 +1,26 @@
+
 from django.urls import path
 from .views.test_views import test_view
-from .views.shopping_carts_view import shopping_carts_view
+from .views.shopping_carts_view import ShoppingCartsView
 from .views.user_profile_view import UserProfileView
 from .views.user_profile_view import CreateUserView
 from .views.user_profile_view import UserDetailView
+from .views.user_profile_view import CreateCreditCardView
+from .views.user_profile_view import UserUpdateView
 from .views.wish_list_books_view import wish_list_books_view
+from .views.books_view import books_view
+from .views.book_details_view import book_details_view
 
-# add route paths here
 urlpatterns = [
     path('test/', test_view),
-    path('user/<int:id>/shopping_carts', shopping_carts_view),
+    path('user/<int:id>/shopping_carts', ShoppingCartsView.as_view()),
     path('user/<int:user_id>/profile', UserProfileView.as_view(), name='user_profile'),
     path('user/create', CreateUserView.as_view(), name='user-create'),
     path('user/', UserDetailView.as_view(), name='user-detail'),
     path('user/<int:id>/wishlist', wish_list_books_view),
+    path('books/', books_view),
+    path('books/<int:id>/details', book_details_view),
+    path('user/create-credit-card', CreateCreditCardView.as_view(), name='create_credit_card'),
+    path('user/<int:user_id>/update', UserUpdateView.as_view(), name='user-update'),
+
 ]
--- djangoBookStoreApi/views/user_profile_view.py
+++ djangoBookStoreApi/views/user_profile_view.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.hashers import make_password
 import json
 
-from ..models.Users import Users
+from ..models.Users import Users, CreditCard
 
 class UserProfileView(View):
     def get(self, request, user_id):
@@ -69,3 +69,72 @@ class UserDetailView(View):
             'home_address': user.home_address,
         }
         return JsonResponse(data)
+
+class CreateCreditCardView(View):
+    def post(self, request):
+        # Extract JSON data from the request body
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            data = {}
+
+        # Extract username and credit card details from the JSON data
+        username = data.get('username')
+        credit_card_number = data.get('credit_card_number')
+        expiration_date = data.get('expiration_date')
+
+        # Check if username and credit card details are provided
+        if username and credit_card_number and expiration_date:
+            # Get the user instance
+            try:
+                user = Users.objects.get(username=username)
+            except Users.DoesNotExist:
+                return JsonResponse({'error': 'User does not exist'}, status=400)
+
+            # Create a new credit card instance
+            credit_card = CreditCard.objects.create(
+                user=user,
+                credit_card_number=credit_card_number,
+                expiration_date=expiration_date
+            )
+
+            data = {
+                'message': 'Credit card created successfully',
+                'credit_card_id': credit_card.id
+            }
+            return JsonResponse(data)
+        else:
+            data = {
+                'error': 'Username, credit card number, and expiration date are required'
+            }
+            return JsonResponse(data, status=400)
+
+class UserUpdateView(View):
+    def put(self, request, user_id):
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            data = {}
+
+        # Retrieve the user object by user_id
+        try:
+            user = Users.objects.get(id=user_id)
+        except Users.DoesNotExist:
+            response = {
+                'error': 'User not found'
+            }
+            return JsonResponse(response, status=404)
+
+        # Update the user fields except for email
+        for key, value in data.items():
+            if key != 'email':
+                setattr(user, key, value)
+
+        # Save the updated user
+        user.save()
+
+        response = {
+            'message': 'User updated successfully',
+            'user_id': user.id
+        }
+        return JsonResponse(response)
